3fc412ad3e4206a81ad65fa632187b4d31805834
imxigh66 <vladakasapcuk@gmail.com>
2026-01-28 03:05:51 +0200

    create users endpoints

---
+++ Application/Users/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTO.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Users.QueriesCommand
+{
+    public class GetUserByIdQuery:IRequest<OperationResult<UserDto>>
+    {
+        public int UserId { get; set; }
+    }
+}
